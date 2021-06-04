c0dcd4cbc21b2baf3765febe4c2414da4d738465
oumayb <bounou.oumayma@gmail.com>
2021-06-04 13:42:49 +0200

    Minr

--- README.md
+++ README.md
@@ -1,26 +1,46 @@
-# Generate data
-A pendulum dataset is provided as a zip file in ``odc/datasets/pendulum_v_low_eval_64_64_bw.zip``. 
-To generate a cartpole dataset, run:
+# Managing environments
 ````
-bash generate_data.sh
+cd online_dynamics_control
+conda env create -f odc_environment.yml --name odc
+conda activate odc
+pip install -e .
 ````
-or edit it for other systems.
 
-# Train model
+A pendulum dataset is provided as a zip file in ``odc/datasets/pendulum_v_low_eval_64_64_bw.zip``. To generate more 
+systems, see section 5.
+
+
+
+# 1. Train model
 ````
 python train.py --data_path datasets/simple_low --lr 1e-3 --ae_out_dim 8 --dataset_fraction 1 \
  --delayed_dmd 1 --history 2 --a_method prox --online_update 0 --exp_name test_run_
 ````
 Checkpoints will be saved in ``odc/runs/``
 
-## Keep only models with lowest losses
+## 1.1 Keep only models with lowest losses
 ````
 bash clean_runs.sh
 ````
 
-## Evaluate model
+## 1.2 Evaluate model
 ````
 python odc/evaluate_model.py --runs_dir odc/runs --exp_name test_run_ --eval 1 \ 
 --sample_duration 300 --metric_prefix sd=300_
 ````
 Complete test_run_ with complete name with date
+
+
+
+# 5. Generate data
+````
+cd online_dynamics_control
+conda env create -f pino_environment.yml --name pino
+conda activate pino
+pip install -e .
+````
+To generate a cartpole dataset, run:
+````
+bash generate_data.sh
+````
+or edit it for other systems.
--- generate_data.sh
+++ generate_data.sh
@@ -2,32 +2,34 @@ mkdir "datasets/cartpole_128_128"
 mkdir "datasets/cartpole_128_128/train"
 mkdir "datasets/cartpole_128_128/test"
 mkdir "datasets/cartpole_128_128/eval"
+conda init bash
+conda activate pino
+echo "Activating pino env"
 
-conda activate odc
 
-python odc/generate_simulations_pinocchio.py --idx_start 0 --idx_end 2000 --target_dir datasets/cartpole_128_128/train/ \
+python odc/generate_simulations_pinocchio.py --idx_start 0 --idx_end 2 --target_dir datasets/cartpole_128_128/train/ \
 --with_cart 1 --max_initial_v 0 --min_bar_length 0.4 --max_bar_length 0.9 --lower_limit 0 --upper_limit 4 \
 --max_initial_v 0 --min_cart_length_radius_ratio 3 --max_cart_length_radius_ratio 7 --duration 5
 
-python odc/generate_simulations_pinocchio.py --idx_start 0 --idx_end 2000 --target_dir datasets/cartpole_128_128/test/ \
+python odc/generate_simulations_pinocchio.py --idx_start 0 --idx_end 2 --target_dir datasets/cartpole_128_128/test/ \
 --with_cart 1 --max_initial_v 0 --min_bar_length 0.4 --max_bar_length 0.9 --lower_limit 0 --upper_limit 4 \
 --max_initial_v 0 --min_cart_length_radius_ratio 3 --max_cart_length_radius_ratio 7 --duration 5
 
-python odc/generate_simulations_pinocchio.py --idx_start 0 --idx_end 1000 --target_dirdatasets/ cartpole_128_128/eval/ \
+python odc/generate_simulations_pinocchio.py --idx_start 0 --idx_end 2 --target_dir datasets/cartpole_128_128/eval/ \
 --with_cart 1 --max_initial_v 0 --min_bar_length 0.4 --max_bar_length 0.9 --lower_limit 0 --upper_limit 4 \
 --max_initial_v 0 --min_cart_length_radius_ratio 3 --max_cart_length_radius_ratio 7 --duration 15
 
-mkdir "cartpole_128_128_bw"
-mkdir "cartpole_128_128_bw/train"
-mkdir "cartpole_128_128_bw/test"
-mkdir "cartpole_128_128_bw/eval"
+mkdir "datasets/cartpole_128_128_bw"
+mkdir "datasets/cartpole_128_128_bw/train"
+mkdir "datasets/cartpole_128_128_bw/test"
+mkdir "datasets/cartpole_128_128_bw/eval"
 
 python odc/dataset_utils/convert_to_bw.py --source_dir datasets/cartpole_128_128/train --target_dir datasets/cartpole_128_128_bw/train/ --out_channels 3
 python odc/dataset_utils/convert_to_bw.py --source_dir datasets/cartpole_128_128/test --target_dir datasets/cartpole_128_128_bw/test/ --out_channels 3
 python odc/dataset_utils/convert_to_bw.py --source_dir datasets/cartpole_128_128/eval --target_dir datasets/cartpole_128_128_bw/eval/ --out_channels 3
 
 
-mkdir "cartpole_64_64_bw"
+mkdir "datasets/cartpole_64_64_bw"
 
 python odc/dataset_utils/resize_dataset.py --source_dir datasets/cartpole_128_128_bw  --target_dir datasets/cartpole_64_64_bw  --size 64
 
